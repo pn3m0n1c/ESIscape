0f8988cd82a2ba17b80b4d539ca860a9c3e11f2b
José Miguel Pérez Tejero <josemiguel.pereztejero@alum.uca.es>
2026-03-10 23:57:23 +0100

    Some work on UI
    Made the underlying system for menu entries and menus as a concept, making it possible for each choice of a menu to call a function or simply return the index of the choice made.

---
+++ ui/ui.h
@@ -0,0 +1,30 @@
+#ifndef UI
+
+#define UI
+
+typedef struct{
+
+    char name[128];
+    void (*action)();
+
+} menu_entry;
+
+typedef struct{
+
+    menu_entry* entries;
+    int number_of_entries;
+    int type;
+
+} menu;
+
+//CABECERA void show_game_name()
+//PRECONDICIÓN
+//POSTCONDICIÓN imprime el nombre del juego "ESI-ESCAPE" en pantalla
+void show_game_name();
+
+//CABECERA: int create_menu(menu menu_to_show)
+//PRECONDICIÓN: menu_to_show preinicializado
+//POSTCONDICIÓN: mostrará en pantalla el menú cargado en menu_to_show, y devolverá el índice de la opción elegida, y ejecutará una función si esta no está definida como nula.
+int create_menu(menu);
+
+#endif
