fb880209e15d2973bd661000009a84d82b1c4fe1
JMIDEV <jmidevcuentas.xd@gmail.com>
2026-03-26 17:04:56 +0100

    Este commit está solo para revertirlo

--- src/main.c
+++ src/main.c
@@ -1,12 +1,11 @@
-#include "partida/partida.h"
+#include "ui/ui.h"
+#include "salas/salas.h"
 
 int main(){
 
-    while(1==1){
-
-        partida_game_start();
-
-    }
+    Salas test_salas = salas_load_salas("./data/Salas.txt");
+    Conns test_conns = salas_load_conns("./data/Conexiones.txt");
+    ui_show_filter_connections(&test_conns, &test_salas, "");
     
     return(0);
 
